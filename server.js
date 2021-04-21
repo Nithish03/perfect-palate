67e43b014f4f209621e61455bd6d6f9cf6907a70
Nithish155 <18eucs074@skcet.ac.in>
2021-04-21 18:14:06 +0530

    Update on recipe registration

--- server.js
+++ server.js
@@ -147,7 +147,6 @@ app.get('/admin/registration',function(req, res) {
     res.render("admin/registration");
 });
 app.get('/admin/categories',function(req, res) {
-     
     res.render("admin/categories");
 });
 app.get('/admin/contact-us',function(req, res) {
@@ -175,25 +174,20 @@ app.get('/admin/login',function(req, res) {
 //     res.writeHead(200, {'Content-Type': 'text/html'});
 //     res.end('Feel free to add query parameters to the end of the url');
 //   }).listen(8080);
+
+app.get('/admin/recipereg',function(req, res) {
+    res.render("admin/recipereg");
+});
+
     app.get('/admin/recipepage', (req,res) => {
         var id=req.query.id;
         console.log(`${id}`);
-        
-        var db = mongoose.connection;
         var collection = db.collection('recipe_post');
+        
         collection.find({'id':id}).toArray(function(err, recipe) {
-          res.render('admin/recipepage', {'recipe_post': recipe})
-          console.log(recipe);
-        
-        var collections = db.collection('review');
-        collections.find({}).toArray(function(err, review) {
-          res.render('admin/recipepage', {'review': review})
-            console.log(review);
-        });  
+        res.render('admin/recipepage', {'recipe_post': recipe})
     });
-        
 });  
-
         // db.collection('recipe_post').find({"id": id}).toArray((err,recipe) => {
         //     if(!err) {
         //         res.render('admin/recipepage', {'recipe_post': recipe});
@@ -432,10 +426,10 @@ app.post('/admin/recipepage', function(req,res){
 })
 
 //review
-// app.get('/',function(req,res) {
-//     res.render(__dirname + "/admin/recipepage",{ details: null })
-//     res.sendFile(__dirname + '/admin/recipepage')
-// });
+app.get('/',function(req,res) {
+    res.render(__dirname + "/admin/recipepage",{ details: null })
+    res.sendFile(__dirname + '/admin/recipepage')
+});
 
 
 
